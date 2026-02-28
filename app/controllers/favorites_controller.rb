359790d44a6d03ac5a9967439e49c725444c6c55
miya38-hub <miyacharge3803@gmail.com>
2026-02-28 08:03:58 +0000

    Finish ouyou4

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,21 @@
 class FavoritesController < ApplicationController
-
   def create
-    book = Book.find(params[:book_id])
-    current_user.favorites.find_or_create_by(book: book)  # 重複防止
-    redirect_back fallback_location: book_path(book)
+    @book = Book.find(params[:book_id])
+    current_user.favorites.find_or_create_by!(book: @book)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: book_path(@book) }
+    end
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    current_user.favorites.where(book: book).destroy_all  # 既に重複してても全部消す
-    redirect_back fallback_location: book_path(book)
-  end
+    @book = Book.find(params[:book_id])
+    current_user.favorites.where(book: @book).destroy_all
 
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: book_path(@book) }
+    end
+  end
 end
